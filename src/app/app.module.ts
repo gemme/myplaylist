c7e05a8d12a678b81334ab8554f8854b1683bbd0
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2019-02-13 18:50:39 -0600

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SongsComponent } from './songs/songs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsComponent
   ],
   imports: [
     BrowserModule
