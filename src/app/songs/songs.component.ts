4839641eb5221b8354e21dfea32dc623989864fe
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2019-02-20 12:11:27 -0600

    laboratorio7 service, http and binding

--- src/app/songs/songs.component.ts
+++ src/app/songs/songs.component.ts
@@ -6,15 +6,23 @@ import { SongService } from './song.service';
   templateUrl: './songs.component.html',
   styleUrls: ['./songs.component.css']
 })
+
 export class SongsComponent implements OnInit {
 
   // instance variables
-  public songs:Array<object> = null;
+  public songs:Array<any> = null;
+  public song:string = null;
 
   constructor(private songService:SongService) {}
 
   ngOnInit() {
     this.songs = this.songService.getSongs();
+  }
+
+  getSongs() {
+    this.songService.searchSong(this.song).then(response => {
+      this.songs = response;
+    });
   }
 
 }
