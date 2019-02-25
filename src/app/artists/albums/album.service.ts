b64f9b10524e96f10fc9fee28dd78e53aaf7d065
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2019-02-25 11:12:57 -0600

    laboratorio9 add API KEY and URL from environment

--- src/app/artists/albums/album.service.ts
+++ src/app/artists/albums/album.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import { environment } from '../../../environments/environment';
+
 type Response = {
   topalbums: {
     album: Album
@@ -17,14 +19,15 @@ type Album = {
 })
 export class AlbumService {
 
-  private API_KEY = '7cc2122f78c55e48c3fe8b33a16b5ef8';
+  private API_KEY = environment.API_KEY;
+  private API_URL = environment.API_URL;
 
   constructor(private http: HttpClient) { }
 
   getAlbums(artist){
-    const API_URL = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=${this.API_KEY}&format=json`;
+    const ALBUMS_URL = `${this.API_URL}method=artist.gettopalbums&artist=${artist}&api_key=${this.API_KEY}&format=json`;
     return this.http
-      .get(API_URL)
+      .get(ALBUMS_URL)
       .pipe(
         map((response: Response) => response.topalbums.album)
       )
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '7cc2122f78c55e48c3fe8b33a16b5ef8',
+  API_URL: 'http://ws.audioscrobbler.com/2.0/?'
 };
 
 /*
